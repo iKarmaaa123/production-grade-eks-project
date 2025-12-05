4747b4e1f0d049a2df659db7b410f44ef6c4d508
iKarmaaa123 <waael@hotmail.co.uk>
2025-12-05 18:08:05 +0000

    feat: added new features to eks project

--- bin/coderco-eks-project.ts
+++ bin/coderco-eks-project.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { ClusterStack } from '../lib/eks-stack';
 import { NetworkingStack } from '../lib/networking-stack';
+import { HelmStack } from "../lib/helm-stack"
 
 const app = new cdk.App();
 
@@ -9,21 +10,31 @@ const networkingStack = new NetworkingStack(app, 'NetworkingStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION
-    }
+  }
 });
 
-const eksStack= new ClusterStack(app, 'ClusterStack', {
-    env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION
-    },
-    vpc: networkingStack.vpc,
-    publicSubnetId: networkingStack.subnetPublicId,
-    publicSubnetId2: networkingStack.subnetPublicId2,
-    privateSubnetId: networkingStack.subnetPrivateId,
-    privateSubnetId2: networkingStack.subnetPrivateId2,
+const clusterStack = new ClusterStack(app, 'ClusterStack', {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  },
+  vpc: networkingStack.vpc,
+  publicSubnetId: networkingStack.subnetPublicId,
+  publicSubnetId2: networkingStack.subnetPublicId2,
+  privateSubnetId: networkingStack.subnetPrivateId,
+  privateSubnetId2: networkingStack.subnetPrivateId2,
 });
 
-eksStack.addDependency(networkingStack)
+const helmStack = new HelmStack(app, "HelmStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  },
+  cluster: clusterStack.cluster,
+  certManagerServiceAccount: clusterStack.certManagerServiceAccount,
+  externalDNSServiceAccount: clusterStack.externalDNSServiceAccount
+})
+
+clusterStack.addDependency(networkingStack);
 
 
--- lib/eks-stack.ts
+++ lib/eks-stack.ts
@@ -7,187 +7,156 @@ import { KubectlV32Layer } from '@aws-cdk/lambda-layer-kubectl-v32';
 import * as Route53 from 'aws-cdk-lib/aws-route53';
 
 interface EKSClusterStackProps extends cdk.StackProps {
-    vpc: ec2.IVpc
-    publicSubnetId: string
-    publicSubnetId2: string
-    privateSubnetId: string
-    privateSubnetId2: string
+  vpc: ec2.IVpc;
+  publicSubnetId: string;
+  publicSubnetId2: string;
+  privateSubnetId: string;
+  privateSubnetId2: string;
 }
 
-const clusterName = "demo-cluster"
-const version = eks.KubernetesVersion.V1_32
-const endpointAccess = eks.EndpointAccess.PUBLIC_AND_PRIVATE.onlyFrom("0.0.0.0/0")
-const defaultCapacity = 2
+const clusterName = "demo-cluster";
+const version = eks.KubernetesVersion.V1_32;
+const endpointAccess = eks.EndpointAccess.PUBLIC_AND_PRIVATE.onlyFrom("0.0.0.0/0");
+const defaultCapacity = 2;
 
 export class ClusterStack extends cdk.Stack {
+
+  public readonly cluster: cdk.aws_eks.Cluster
+  public readonly certManagerServiceAccount: cdk.aws_eks.ServiceAccount
+  public readonly externalDNSServiceAccount: cdk.aws_eks.ServiceAccount
+
   constructor(scope: Construct, id: string, props: EKSClusterStackProps) {
     super(scope, id, props);
 
-const accountId = this.account
+    const accountId = this.account;
 
-const kubernetesApiAccessPolicy = new iam.PolicyStatement({
-  actions: [
-    "eks:AccessKubernetesApi",
-    "eks:DescribeCluster"
-  ],
-  resources: [
-    `arn:aws:eks:*:${accountId}:cluster/*`
-  ]
-    })
+    const kubernetesApiAccessPolicy = new iam.PolicyStatement({
+      actions: [
+        "eks:AccessKubernetesApi",
+        "eks:DescribeCluster"
+      ],
+      resources: [
+        `arn:aws:eks:*:${accountId}:cluster/*`
+      ]
+    });
 
-const EKSClusterMasterRole = new iam.Role(this, "ClusterMasterRole", {
-  assumedBy: new iam.AccountPrincipal(accountId),
-  roleName: "EksClusterMasterRole",
-  inlinePolicies: {
-    "KubernetesApiAccess": new iam.PolicyDocument({
-      statements: [kubernetesApiAccessPolicy]
-    })
-  }
-})
-
-const cluster = new eks.Cluster(this, "HelloEKS", {
-  vpc: props.vpc,
-  clusterName: clusterName,
-  mastersRole: EKSClusterMasterRole,
-  version: version,
-  endpointAccess: endpointAccess,
-  vpcSubnets: [{ subnetType: ec2.SubnetType.PUBLIC },
-               { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS}],
-  defaultCapacity: defaultCapacity,
-  kubectlLayer: new KubectlV32Layer(this, "kubectl"),
-});
-
-const certManagerNameSpace = cluster.addManifest("cert-manager", {
-  apiVersion: "v1",
-  kind: "Namespace",
-  metadata: {name: "cert-manager"}
-})
-
-const externalDNSNameSpace = cluster.addManifest("external-dns", {
-  apiVersion: "v1",
-  kind: "Namespace",
-  metadata: {name: "external-dns"}
-})
-
-const zone = Route53.HostedZone.fromLookup(this, "HostedZone", {
-  domainName: "cdk-labs.com"
-})
-
-const certManagerServiceAccount = cluster.addServiceAccount("cert-manager", {
-  name: "cert-manager",
-  namespace: "cert-manager",
-  identityType: eks.IdentityType.IRSA,
-  labels: {
-    "app.kubernetes.io/managed-by": "Helm"
-  },
-  annotations: {
-    "meta.helm.sh/release-name": "cert-manager",
-    "meta.helm.sh/release-namespace": "cert-manager"
-  }
-})
-certManagerServiceAccount.node.addDependency(certManagerNameSpace)
-
-const externalDNSManagerServiceAccount = cluster.addServiceAccount("external-dns", {
-  name: "external-dns",
-  namespace: "external-dns",
-  identityType: eks.IdentityType.IRSA,
-  labels: {
-    "app.kubernetes.io/managed-by": "Helm"
-  },
-  annotations: {
-    "meta.helm.sh/release-name": "external-dns",
-    "meta.helm.sh/release-namespace": "external-dns"
-  }
-})
-externalDNSManagerServiceAccount.node.addDependency(externalDNSNameSpace)
-
-certManagerServiceAccount.role.addToPrincipalPolicy(new iam.PolicyStatement({
-  actions: [
-    "route53:GetChange",
-    "route53:ChangeResourceRecordSets",
-    "route53:ListResourceRecordSets",
-    "route53:ListHostedZonesByName",
-    "route53:ListHostedZones"
-],  
-  resources: [ zone.hostedZoneArn ],
-}));
-
-externalDNSManagerServiceAccount.role.addToPrincipalPolicy(new iam.PolicyStatement({
-  actions: [
-    "route53:GetChange",
-    "route53:ChangeResourceRecordSets",
-    "route53:ListResourceRecordSets",
-    "route53:ListHostedZonesByName",
-    "route53:ListHostedZones"
-],
-  resources: [ zone.hostedZoneArn ],
-}));
-
-cluster.addHelmChart("nginx", {
-  chart: "ingress-nginx",
-  repository: "https://kubernetes.github.io/ingress-nginx",
-  release: "ingress-nginx",
-  namespace: "nginx",
-  version: "4.13.3",
-  createNamespace: true,
-  wait: true,
-  values: {
-    installCRDs: true
-  },
-})
-
-cluster.addHelmChart("cert-manager", {
-  chart: "cert-manager",
-  repository: "https://charts.jetstack.io",
-  release: "cert-manager",
-  namespace: "cert-manager",
-  version: "1.13.2",
-  createNamespace: false,
-  wait: true,
-  values: {
-    installCRDs: true,
-    "serviceAccount.create": false,
-    "serviceAccount": {
-      "annotations": {
-        "eks.amazonaws.com/role-arn": certManagerServiceAccount.role.roleArn,
-      },
-    },
-    "podAnnotations": {
-      "eks.amazon.com/role-arn": certManagerServiceAccount.role.roleArn
-    },
-    "ingressShim.defaultIssuerKind": "dns01",
-    "ingressShim.defaultIssuerProvider": "route53",
-    "extraArgs[0]": "--dns01-recursive-nameservers=8.8.8.8:53",
-    "extraArgs[1]": "--dns01-recursive-nameservers-only",
-    domainFilters: ["cdk-labs.com"],
-    region: "us-east-1"
-  }
-}).node.addDependency(certManagerNameSpace)
-
-cluster.addHelmChart("external-dns", {
-  chart: "external-dns",
-  repository: "https://kubernetes-sigs.github.io/external-dns/",
-  release: "external-dns",
-  namespace: "external-dns",
-  version: "1.19.0",
-  createNamespace: false,
-  wait: true,
-  values: {
-    installCRDs: true,
-    "serviceAccount.create": false,
-    "serviceAccount": {
-      "annotations": {
-        "eks.amazonaws.com/role-arn": externalDNSManagerServiceAccount.role.roleArn
+    const EKSClusterMasterRole = new iam.Role(this, "ClusterMasterRole", {
+      assumedBy: new iam.AccountPrincipal(accountId),
+      roleName: "EksClusterMasterRole",
+      inlinePolicies: {
+        "KubernetesApiAccess": new iam.PolicyDocument({
+          statements: [kubernetesApiAccessPolicy]
+        })
       }
-    },
-    "podAnnotations": {
-      "eks.amazonaws.com/role-arn": externalDNSManagerServiceAccount.role.roleArn,
-    },
-    env: [{
-      name: "AWS_DEFAULT_REGION",
-      value: "us-east-1"
-    }]
+    });
+
+    this.cluster = new eks.Cluster(this, "HelloEKS", {
+      vpc: props.vpc,
+      clusterName: clusterName,
+      mastersRole: EKSClusterMasterRole,
+      version: version,
+      endpointAccess: endpointAccess,
+      vpcSubnets: [
+        { subnetType: ec2.SubnetType.PUBLIC },
+        { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }
+      ],
+      defaultCapacity: defaultCapacity,
+      kubectlLayer: new KubectlV32Layer(this, "kubectl"),
+    });
+
+    const certManagerNamespace = this.cluster.addManifest("cert-manager", {
+      apiVersion: "v1",
+      kind: "Namespace",
+      metadata: { name: "cert-manager" }
+    });
+
+    const externalDNSNamespace = this.cluster.addManifest("external-dns", {
+      apiVersion: "v1",
+      kind: "Namespace",
+      metadata: { name: "external-dns" }
+    });
+
+    const zone = Route53.HostedZone.fromLookup(this, "HostedZone", {
+      domainName: "cdk-labs.com"
+    });
+
+    this.certManagerServiceAccount = this.cluster.addServiceAccount("cert-manager", {
+      name: "cert-manager",
+      namespace: "cert-manager",
+      identityType: eks.IdentityType.IRSA,
+      labels: {
+        "app.kubernetes.io/managed-by": "Helm"
+      },
+      annotations: {
+        "meta.helm.sh/release-name": "cert-manager",
+        "meta.helm.sh/release-namespace": "cert-manager"
+      }
+    });
+    this.certManagerServiceAccount.node.addDependency(certManagerNamespace);
+
+    this.externalDNSServiceAccount = this.cluster.addServiceAccount("external-dns", {
+      name: "external-dnstwo",
+      namespace: "external-dns",
+      identityType: eks.IdentityType.IRSA,
+      labels: {
+        "app.kubernetes.io/managed-by": "Helm"
+      },
+      annotations: {
+        "meta.helm.sh/release-name": "external-dns",
+        "meta.helm.sh/release-namespace": "external-dns"
+      }
+    });
+    this.externalDNSServiceAccount.node.addDependency(externalDNSNamespace);
+
+    this.certManagerServiceAccount.role.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: [
+        "route53:GetChange",
+        "route53:ChangeResourceRecordSets",
+        "route53:ListResourceRecordSets",
+        "route53:ListHostedZonesByName",
+        "route53:ListHostedZones"
+      ],
+      resources: ["*"],
+    }));
+
+    this.externalDNSServiceAccount.role.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: [
+        "route53:GetChange",
+        "route53:ChangeResourceRecordSets",
+        "route53:ListResourceRecordSets",
+        "route53:ListHostedZonesByName",
+        "route53:ListHostedZones"
+      ],
+      resources: ["*"],
+    }));
+
+     new eks.HelmChart(this, "cert-manager", {
+        cluster: this.cluster,
+        chart: "cert-manager",
+        repository: "https://charts.jetstack.io",
+        release: "cert-manager",
+        namespace: "cert-manager",
+        version: "1.13.2",
+        createNamespace: false,
+        wait: true,
+        values: {
+          installCRDs: true,
+          "serviceAccount.create": false,
+          "serviceAccount.name": this.certManagerServiceAccount.serviceAccountName,
+          "serviceAccount": {
+            "annotations": {
+              "eks.amazonaws.com/role-arn": this.certManagerServiceAccount.role.roleArn,
+            },
+          },
+          "ingressShim.defaultIssuerKind": "dns01",
+          "ingressShim.defaultIssuerProvider": "route53",
+          "extraArgs[0]": "--dns01-recursive-nameservers=8.8.8.8:53",
+          "extraArgs[1]": "--dns01-recursive-nameservers-only",
+          "domainFilters": ["cdk-labs.com"],
+          "region": "us-east-1"
+        }
+      }).node.addDependency(certManagerNamespace)
   }
-}).node.addDependency(externalDNSNameSpace)
- }
 }
+
+
