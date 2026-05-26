fb75be99f35642e7716393b0353cff78adf1034b
iKarmaa123 <waael@hotmail.co.uk>
2026-05-26 19:16:27 +0100

    chore: removed dotenv dependency and env props from cdk app file

--- infra/bin/eks-project.ts
+++ infra/bin/eks-project.ts
@@ -1,17 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { ClusterStack } from '../lib/eks-stack';
 import { HelmStack } from '../lib/helm-stack';
-import dotenv from "dotenv"
 
 const app = new cdk.App();
-const eksStack = new ClusterStack(app, "ClusterStack", {
-    env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION
-  }});
-
-const helmStack = new HelmStack(app, "HelmStack", eksStack, {
-    env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION
-  }});
+const eksStack = new ClusterStack(app, "ClusterStack");
+const helmStack = new HelmStack(app, "HelmStack", eksStack);
